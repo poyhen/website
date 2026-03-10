169456170d714edf8e9752487882359c7c598537
poyhen <poyhen@proton.me>
2026-03-10 21:51:33 +0300

    switch to rust compiler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-    },
-
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    rustCompiler: true,
+  },
   integrations: [react()],
   adapter: cloudflare(),
 });
